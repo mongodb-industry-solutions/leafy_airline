4791da8a1f90ad8ce39365e8e0fd21d7500e51a4
Tamar Alphaidze <talphaidze.ieu2020@student.ie.edu>
2024-07-09 09:21:33 +0200

    test CI/CD

--- airplanedashboard/pages/index.js
+++ airplanedashboard/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <Layout>
       <h2>Content Area</h2>
       <p>This is where the main content will go.</p>
-      <p>This is a test for automated CI/CD in deployment</p>
+      <p>This is another test for automated CI/CD in deployment</p>
     </Layout>
   );
 }
