15062ef010671db7518f9e94ca8eadb8ca3c2178
Tamar Alphaidze <talphaidze.ieu2020@student.ie.edu>
2024-07-17 09:52:45 +0200

    added page

---
+++ airplanedashboard/components/Layout1.js
@@ -0,0 +1,22 @@
+// components/Layout.js
+import React from 'react';
+import styles from './Layout.module.css';
+import Logo from '@leafygreen-ui/logo';
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>Flight ID: 991345</h1>
+        <Logo className={styles.logo} />
+      </header>
+      <div className={styles.main}>
+        <div className={styles.content}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ airplanedashboard/pages/index1.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import React from 'react';
+import Layout from '../components/Layout1';
+
+export default function Home() {
+  return (
+    <Layout>
+      <h2>Content Area</h2>
+      <p>This is where the main content will go.</p>
+    </Layout>
+  );
+}
