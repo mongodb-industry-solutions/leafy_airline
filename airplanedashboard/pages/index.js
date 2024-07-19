499a593824922953379466cc90a6d3a2b70e96af
Patricia <paty.renartcarnicero@mongodb.com>
2024-07-19 17:12:43 +0200

    Update in app architecture

--- airplanedashboard/pages/index.js
+++ airplanedashboard/pages/index.js
@@ -2,12 +2,19 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import SearchBar from '../components/searchbar'; // Import the SearchBar component
+import FilterSection from '../components/FilterSection';
+import styles from '../components/Layout.module.css';
 
 export default function Home() {
   return (
     <Layout>
       {/* Integrate the SearchBar component here */}
-      <SearchBar />
+      <aside className={styles.sidebar}>
+        <FilterSection/>
+      </aside>
+      <div className={styles.content}>
+        <SearchBar />
+      </div>
     </Layout>
   );
 }
