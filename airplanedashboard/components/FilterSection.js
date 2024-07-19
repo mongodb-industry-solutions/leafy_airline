787e2d7103062eeac36304c4e8d0936ab0511bd9
Tamar Alphaidze <talphaidze.ieu2020@student.ie.edu>
2024-07-19 12:30:10 +0200

    modules

--- airplanedashboard/components/FilterSection.js
+++ airplanedashboard/components/FilterSection.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styles from './Layout.module.css';
 import Button from '@leafygreen-ui/button';
 
--- airplanedashboard/components/Layout.js
+++ airplanedashboard/components/Layout.js
@@ -1,9 +1,9 @@
-// components/Layout.js
 import React from 'react';
 import styles from './Layout.module.css';
-import Logo from '@leafygreen-ui/logo';
-import Icon from "@leafygreen-ui/icon";
-import FilterSection from './FilterSection';
+import Logo from '@leafygreen-ui/logo';  // Ensure these are correct
+import Icon from '@leafygreen-ui/icon';  // Ensure these are correct
+import FilterSection from './FilterSection';  // Ensure this component is correctly exported
+import Button from '@leafygreen-ui/button';
 
 const Layout = ({ children }) => {
   return (
@@ -11,8 +11,8 @@ const Layout = ({ children }) => {
 
       <header className={styles.header}>
         <div className={styles.icon_container}>
-            <Icon glyph='Person' fill="#00684A" size='xlarge'/>
-            <span>Flight Operations Manager</span>
+          <Icon glyph='Person' fill="#00684A" size='xlarge'/>
+          <span>Flight Operations Manager</span>
         </div>
         <h1>Leafy Airline Dashboard</h1>
         <Logo className={styles.logo} />
@@ -21,7 +21,6 @@ const Layout = ({ children }) => {
       <div className={styles.main}>
         
         <aside className={styles.sidebar}>
-          <FilterSection />
         </aside>
 
         <div className={styles.content}>
