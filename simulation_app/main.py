a56a2c5e3440d746144bf714a451cddd33a7ace2
Patricia <paty.renartcarnicero@mongodb.com>
2024-07-26 10:52:19 +0200

    Fix gitignore

---
+++ simulation_app/main.py
@@ -0,0 +1,197 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from simulator import DataSimulator
+from path_finder import find_path
+from apscheduler.schedulers.background import BackgroundScheduler
+import logging
+import os
+import json
+from google.cloud import pubsub_v1
+
+# Resources:
+# https://medium.com/@mouaazfarrukh99/getting-started-with-pub-sub-using-python-305a19901f1a
+# https://www.youtube.com/watch?v=ML6P1ksHcqo&list=PLIivdWyY5sqKwVLe4BLJ-vlh9r9zCdOse&index=4 
+
+
+# MongoDB connection : Data API - Custom endpoints
+
+
+# INITIALIZE THE APP WITH COMMAND : fastapi dev main.py
+app = FastAPI()
+
+origins = [
+    "http://localhost:3001",
+    "http://localhost:3000",  # Next.js app address
+    "https://airplanedashboard-65jcrv6puq-ew.a.run.app/"
+    # Add other origins if needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# LOGGING CONFIG TO ANALYZE DATA
+logging.basicConfig(
+    filename='logs/app.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+
+# SCHEDULER : Calls my function (simulator) every x seconds
+measurement_interval = 1
+scheduler = BackgroundScheduler()
+scheduler_active = False
+resume_needed = False
+docs = []
+
+# PUBSUB INFO
+project_id = "connected-aircraft-ist"
+topic_id = "flight_data"
+service_account_file = "json-keys-for-connect-aircraft-ist/connected-aircraft-ist-4fa26b67848a.json"
+
+publisher = pubsub_v1.PublisherClient.from_service_account_file(service_account_file)
+topic_path = publisher.topic_path(project_id, topic_id)
+
+# GENERAL LIMITS
+doc_limit = 200
+
+
+# FUNCTIONS
+def publish_data(simulator : DataSimulator):
+
+    (finished, data )= simulator.generate_data()
+
+    if len(docs) <= doc_limit:
+        docs.append(data)
+
+    if finished:
+
+        global scheduler
+        global scheduler_active
+
+        logging.info("ARRIVED TO DESTINATION")
+        scheduler.shutdown()
+        scheduler_active = False
+        logging.info("Scheduler stopped due to finished flight")
+
+    # Uncomment when using pubsub
+    data = json.dumps(data).encode("utf-8")
+    future = publisher.publish(topic_path, data)
+    logging.info(future)
+
+    return {"status": "New data published"}
+
+
+# ENDPOINTS FOR FAST API APP 
+
+@app.post("/start-scheduler")
+async def start_scheduler(flight_info:dict):
+    '''
+    This function will trigger the start of the data simulator using the data
+    for the selected flight.
+    This data will be provided in the POST call that will trigger this function
+    and it will be a dictionary containing:
+        - flight_id
+        - dep_code
+        - arr_code
+        - dep_loc
+        - arr_loc
+
+    This data will be the one from the flight that we had selected in our tab
+
+    This function will first find the new path for our flight (taking
+    disruption into account). 
+    Then it will instantiate the DataSimulator for our current flight
+    anf finally will start the scheduler that will begin to produce data
+    calling the generate_data function from our simulator every 5 seconds
+    '''
+    logging.info("Data received")
+
+    # Find the path between the departure and arrival locations
+    (disrupted, path_data) = find_path(flight_info)
+
+    # Create our Data Simulator for this flight
+    simulator = DataSimulator(flight_info["flight_id"],
+                              disruption = disrupted,
+                              path_atrib = path_data, 
+                              seconds_per_iter= 10)
+    
+    logging.info("Simulator created")
+
+    global scheduler
+    global scheduler_active
+    global resume_needed
+
+    # Start the scheduler that will get the data every second
+    if not scheduler_active:
+
+        if not resume_needed:
+            scheduler.add_job(publish_data, 'interval', 
+                            seconds = measurement_interval ,
+                            args = (simulator,)) 
+            scheduler.start()
+            scheduler_active = True
+            logging.info("Scheduler started")
+            return {"status": "Scheduler started"}
+
+        else:
+            scheduler.resume()
+            logging.info("Scheduler resumed")
+            resume_needed = False
+            scheduler_active = True
+            return {"status": "Scheduler resumed"}
+    
+    else:
+        return {"status": "Scheduler already running"}
+
+@app.get("/pause-scheduler")
+async def pause_scheduler():
+    '''
+    This function will trigger the stop of the scheduler that was calling periodically
+    our get_measurements function'''
+
+    global scheduler
+    global scheduler_active
+    global resume_needed
+
+    if scheduler_active:
+
+        scheduler.pause()
+        scheduler_active = False
+        resume_needed = True
+
+        logging.info("Scheduler paused")
+        # Create json doc with all the records
+        logging.info(f"Docs recorded {len(docs)}")
+
+        with open(f'sim_data/flight_telemetry_data.json', 'w') as f:
+            json.dump(docs, f, indent=4)
+        
+        return {"status": "Scheduler paused"}
+    
+    return {"status": "Scheduler was already not active"}
+
+@app.get("/reset-scheduler")
+async def reset_scheduler():
+
+    # Shutdown previous scheduler and create the new one
+    global scheduler
+    global scheduler_active
+    global resume_needed
+
+    scheduler.shutdown()
+
+    scheduler_active = False
+    resume_needed = False
+    scheduler = BackgroundScheduler()
+
+    return {"status": "Reset complete"}
+
+
+
+
---
+++ simulation_app/simulator.py
@@ -0,0 +1,259 @@
+import os
+import numpy as np 
+from datetime import datetime
+import math
+from pyproj import CRS, Transformer
+
+# Define the WGS84 projection (latitude and longitude)
+wgs84 = CRS.from_epsg(4326)
+# Define the Mercator projection (x, y)
+mercator = CRS.from_epsg(3857)
+
+
+class SpeedSimulator:
+    def __init__(self):
+        '''
+        Class that will smimulate the speed controls from a plane based on distance 
+        to arrival.
+
+        Using the 3:1 nautic rule, this class will compute the speed at which the
+        aircraft should fly in each instant. 
+
+        Assumed limit altitude : 30.000 feet (=9200m)
+        3:1 calculated distance to limit altitude : 170 km
+
+        '''
+
+        self.prev_speed = 0
+        self.avg_plane_speed = 245 #245 # m/s -> 885 km/h
+        self.speed_descent_rate = 0.01 # m/s (18 km/h)-> 4 min to go back to 0 m/s
+        self.approach_dist = 170 # km (Based on 0.245 km/s advance, we would need 11.56 min to descend)
+
+
+    def get_new_speed(self, new_dist):
+
+        # If the distance to arrival is equal or less than the approach_dist
+        # we start the "descent"
+        speed = self.prev_speed
+
+        if new_dist <= self.approach_dist:
+            if speed > 100:
+                speed -= self.speed_descent_rate
+            else:
+                speed = 100
+        else:
+            # We add a slight noise to our speed calculations
+            speed = self.avg_plane_speed + np.random.uniform(-5, 5)
+
+        return speed  #*3.6 #Speed in km/h
+
+class CoordinateTransformer:
+
+    def __init__(self, ):
+
+        self.to_2d_transformer = Transformer.from_crs(wgs84, mercator)
+        self.to_latlon_transformer = Transformer.from_crs(mercator, wgs84)
+
+        return
+    
+    def latlon_to_2d(self, lat, lon):
+        x, y = self.to_2d_transformer.transform(lat, lon)
+        return x, y
+
+    def _2d_to_latlon(self, x, y):
+        lat, lon = self.to_latlon_transformer.transform(x, y)
+        return lat, lon
+
+    def unit_vector(self, origin, destination):
+        # Calculate the vector from origin to destination
+        vector = (destination[0] - origin[0], destination[1] - origin[1])
+        # Calculate the magnitude of the vector
+        magnitude = math.sqrt(vector[0]**2 + vector[1]**2)
+        # Normalize the vector
+        if magnitude == 0:
+            return (0, 0)
+        unit = (vector[0] / magnitude, vector[1] / magnitude)
+        return unit
+    
+    def compute_new_loc(self, origin_lat, origin_lon, dest_lat, dest_lon, advancement):
+        #Transform the coordinates from lat/lon to 2D
+        origin_2d = self.latlon_to_2d(origin_lat, origin_lon)
+        destination_2d = self.latlon_to_2d(dest_lat, dest_lon)
+        
+        # Calculate the unit vector from origin to destination
+        unit = self.unit_vector(origin_2d, destination_2d)
+        
+        # Calculate the displacement vector for 1 km
+        displacement = (unit[0] * advancement, unit[1] * advancement)
+        
+        # Add the displacement vector to the origin
+        new_2d = (origin_2d[0] + displacement[0], origin_2d[1] + displacement[1])
+        
+        # Transform the new 2D coordinates back to lat/lon
+        new_lat, new_lon =self._2d_to_latlon(new_2d[0], new_2d[1])
+        
+        return (new_lat, new_lon)
+
+class DataSimulator:
+
+    def __init__(self, flight_ID : str, disruption : bool,  path_atrib: dict, seconds_per_iter: int):
+        
+        self.FID = flight_ID
+        self.disruption = disruption
+        self.path = path_atrib["path"]
+        self.extra_length = path_atrib["extra_length"]
+        self.arrived = False
+        
+        # Get the initial headed point
+        self.headed_point = np.array(self.path[1])
+        self.path_idx = 1
+
+        self.prev_speed = 0
+        self.prev_location = np.array(self.path[0])
+        self.timestamp = datetime.now()
+
+        # Avg aircraft speed is usually between 880 - 930 km/h -> 247,22 m/s
+        self.SpController = SpeedSimulator()
+        self.location_th = 1 # If the plane is at a 1km or less distance from the headed point, it is considered as reached
+        self.advancement_rate = 245*seconds_per_iter # Indicates the meters that the plane can cover per iteration
+        # Coordinate transformer
+        self.CoordTransformer = CoordinateTransformer()
+
+    def get_real_distance(self, loc1, loc2) :
+        '''
+        Haversine function to calculate distance between two points in the earth
+        '''
+        (lat1, lon1) = loc1
+        (lat2, lon2) = loc2
+        lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+        c = 2 * math.asin(math.sqrt(a))
+        km = 6371 * c
+        return km
+
+    def dist_to_arrival(self, loc):
+        '''
+        This function computes the distance to arrival based on the 
+        followed route. If it has been a disruption, this distance will
+        be computed using the new path
+        '''
+        # Get the idx of the current headed point in the path
+        path_idx = self.path_idx
+
+        # If we're just going from A to B, get real straight-line distance to arrival
+        if len(self.path) == 2:
+            return self.get_real_distance(loc, self.path[-1])
+        else:
+            # If there are more than 1 points in our route, iterate through them
+            # so we get the real distance that we have to cover using the extended path
+            total_length = 0
+            for i in range(path_idx, len(self.path)):
+                total_length += self.get_real_distance(loc, self.path[i])
+                loc = self.path[i]
+
+            return total_length
+
+    def new_headed_point(self):
+        '''
+        Function that will check which point of the path we're
+        heading next
+        '''
+        path_len = len(self.path)
+        i = self.path_idx
+
+        # First we check if the point to which we are arriving is the final goal
+        if i == path_len:
+            self.arrived = True
+            self.headed_point = None
+        
+        # If not, we can see if there are more than two main points in our path
+        elif path_len == 2:
+            self.headed_point = self.path[-1]
+        
+        else:
+            self.headed_point = self.path[i+1]
+
+        self.path_idx += 1
+
+        return 
+
+    def generate_data(self):
+        '''
+        This function will generate real-time simulated data based on the route that
+        the aircraft is following (path)
+
+        Returns:
+        1. finished : Boolean that states if we've reached our arrival point
+        2. Simulated data : dict containing
+            - flight_id
+            - ts 
+            - disrupted : Boolean that shows if the route was disrupted
+            - extra_length : Extra length to cover because of disruption (in km)
+            - distance_to_arrival : Distance to arrival location based on route (in km)
+            - location (lat , long)
+            - velocity : speed: new_speed,
+                        heading: new_heading
+        '''
+
+        # 1. Compute direction vector from current position to headed point so we know
+        # in which direction the airplane should move
+        print('\nNew measurements: ')
+        print('Headed', self.headed_point)
+        print('Prev', self.prev_location)
+
+        # Distance to next point in km
+        distance_to_headed = self.get_real_distance(self.prev_location, self.headed_point)
+
+        # Compute next location
+        if distance_to_headed < self.location_th :
+            new_loc = self.headed_point
+            self.new_headed_point()
+
+        else:
+            # If the headed point is not reached, we just compute new location based of previous speed
+            new_loc = self.CoordTransformer.compute_new_loc(self.prev_location[0],
+                                                            self.prev_location[1],
+                                                            self.headed_point[0],
+                                                            self.headed_point[1], 
+                                                            self.advancement_rate )
+
+        print('Distance to headed: ', distance_to_headed)
+        print('Heading to:', self.headed_point)
+        print('From : ', new_loc)
+
+        # 5. Compute new heading direction and new distance to arrival (using the described path)
+        # new_heading = (np.degrees(np.arctan2(unit_vector[1], unit_vector[0])) + 360) % 360
+        distance_to_dest = self.dist_to_arrival(new_loc)
+
+        # Compute new speed 
+        new_speed = self.SpController.get_new_speed(distance_to_dest)
+
+        print('Speed:', new_speed)
+        
+
+        # Update every measurement
+        self.prev_location = new_loc
+        self.prev_speed = new_speed
+        self.timestamp = datetime.now()
+
+
+        return (self.arrived, {
+                "flight_id": self.FID,
+                "ts": self.timestamp.isoformat(),
+                "path" : self.path,
+                "disrupted" : self.disruption,
+                "extra_length" : self.extra_length,
+                "distance_to_arrival" : distance_to_dest,
+                "location": {
+                    "lat": new_loc[0],
+                    "long": new_loc[1]
+                },
+                "velocity": {
+                    "speed": new_speed,
+                    "heading": 'tbd'
+                }
+            })
+
+
