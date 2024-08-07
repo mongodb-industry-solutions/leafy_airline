5c20e8ff72c377f90910483d7a2043fec57ae6f1
Patricia <paty.renartcarnicero@mongodb.com>
2024-08-07 17:02:53 +0200

    Footer slight changes

--- airplanedashboard/components/Layout1.js
+++ airplanedashboard/components/Layout1.js
@@ -371,7 +371,7 @@ const Layout1 = ({ children }) => {
         <div className={footerStyles.footerContent}>
           <p>&copy; 2024 MongoDB. All rights reserved.</p>
           <p>
-            <a href="/privacy-policy">Privacy Policy</a> | <a href="/terms-of-service">Terms of Service</a>
+          Leafy Air Demo developed by IS Team 
           </p>
         </div>
         <div className={footerStyles.footerImage}>
