b531fae74410b8cd0bf3061a8cb6473478970053
Patricia <paty.renartcarnicero@mongodb.com>
2024-08-08 15:08:15 +0200

    change in searchbar name

--- airplanedashboard/pages/index.js
+++ airplanedashboard/pages/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import GeneralLayout from '../components/GeneralLayout';
 import FlightList from '../components/FlightList';
-import SearchBar from '../components/Searchbar';
+import SearchBar from '../components/searchbar';
 import FilterSection from '../components/FilterSection';
 import styles from '../components/GeneralStyle.module.css';
 import { useState, useEffect } from 'react';
