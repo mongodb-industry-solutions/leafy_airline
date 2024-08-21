77dfe62f167eb7fd627af4eefe708ce9a47e6b52
Tamar Alphaidze <talphaidze.ieu2020@student.ie.edu>
2024-08-21 10:49:31 +0200

    directly fetching API key

--- airplanedashboard/components/FlightLayout.js
+++ airplanedashboard/components/FlightLayout.js
@@ -53,6 +53,8 @@ const FlightLayout = ({ children }) => {
   const [newDisrup, setDisruption] = useState({})
   const [disrupEmpty, setDisrupEmpty] = useState(true)
 
+  const googleAPI = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+
   async function fetchData() {
     try {
       const res = await fetch('/api/flights');
@@ -440,9 +442,9 @@ const FlightLayout = ({ children }) => {
           {/* Google Map Component */}
           <div className={styles.rightContainer}>
             <div className={styles.mapContainer}>
-            {apiKey ? (
+            {googleAPI ? (
             <div style={{ position: 'relative', width: '100%', height: '85%' }}>
-              <LoadScript googleMapsApiKey={apiKey}>
+              <LoadScript googleMapsApiKey={googleAPI}>
                 <GoogleMap
                   mapContainerStyle={{ width: '100%', height: '100%' }}
                   center={airplanePosition || depCoords}
